d480e1c484cb81add52cea7e1d5c1a80250e218d
pandazxx <pandazxx@gmail.com>
2024-04-08 21:04:53 +0800

    First commit

---
+++ homelab/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+
+resource "proxmox_lxc" "basic" {
+  target_node  = "pve"
+  hostname     = "lxc-basic"
+  ostemplate   = "local:vztmpl/ubuntu-23.04-standard_23.04-1_amd64.tar.zst"
+  password     = "BasicLXCContainer"
+  unprivileged = true
+
+  // Terraform will crash without rootfs defined
+  rootfs {
+    storage = "local-zfs"
+    size    = "8G"
+  }
+
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+}
